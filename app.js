be21753f94e2e38eddc38747468336569b56547e
João F. Ramos <joao.ramos1988@outlook.com>
2021-11-30 23:34:19 -0300

    Abrindo conexão via websocket

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* importar as configurações do servidor */
+const { on } = require('./config/server');
 var app = require('./config/server');
 
 /* Parametrizar a porta de escuta */
@@ -7,4 +8,14 @@ var server = app.listen(8089, function () {
     console.log('Servidor online');    
 })
 
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+/* criar a conexão por websocket */
+io.on('connection', function(socket){
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function(){
+        console.log("usuário dsconecou");
+    });
+    
+});
