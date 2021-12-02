83c036bb34162a78371bd970b8d0f420e0c1922e
João F. Ramos <joao.ramos1988@outlook.com>
2021-12-01 23:00:06 -0300

    Atualizando  relação entre participantes da conversa

--- app.js
+++ app.js
@@ -15,13 +15,23 @@ app.set('io', io);
 io.on('connection', function(socket){
     console.log('Usuário conectou');
 
+
     socket.on('disconnect', function(){
         console.log("usuário dsconecou");
     });
 
     socket.on('msgParaServidor', function(data){
+        //diálogo
         socket.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem});
         socket.broadcast.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem});
+
+        //participantes
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit('participantesParaCliente', {apelido: data.apelido});
+            socket.broadcast.emit('participantesParaCliente', {apelido: data.apelido});
+        }
+      
+
     });
     
 });
